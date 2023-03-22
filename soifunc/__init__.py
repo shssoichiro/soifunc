40c8a53d1c4d93627d7cd1d5939908aaed8767ac
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2023-03-22 20:34:11 +0100

    Add noqa to re-exports

--- soifunc/__init__.py
+++ soifunc/__init__.py
@@ -1,4 +1,4 @@
-from .deband import *
-from .deint import *
-from .denoise import *
-from .resize import *
+from .deband import *  # noqa: F401, F403
+from .deint import *  # noqa: F401, F403
+from .denoise import *  # noqa: F401, F403
+from .resize import *  # noqa: F401, F403
